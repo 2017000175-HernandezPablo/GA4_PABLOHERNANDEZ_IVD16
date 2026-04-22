ad678e7703087c8d31933684695230ab5dfad6ab
2017000175-HernandezPablo <p.hernandez.5cdb@gmail.com>
2026-04-22 13:47:05 -0600

    GA4 EJER1

---
+++ GA4_PABLOHERNANDEZ_IVD16/src/JOption.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LABORATORIO
+ */
+class JOption {
+
+    static void showInputDialog(String dgite_valor_en_metros_) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
